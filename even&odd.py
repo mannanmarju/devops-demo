56f953201357b2f37337d5b8994b94768a738144
Marzia Mannan <mannanmarju@gmail.com>
2024-12-02 20:28:48 -0800

    add new code

---
+++ even&odd.py
@@ -0,0 +1,10 @@
+print("Even numbers:")
+for i in range(1, 11):
+    if i % 2 == 0:  # Check if the number is even
+        print(i)
+        
+
+print("\nOdd numbers:")
+for i in range(1, 11):
+    if i % 2 != 0:  # Check if the number is odd
+        print(i)
